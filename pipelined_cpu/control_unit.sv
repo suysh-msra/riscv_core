22b997100f81ee7f43f90fc37b0548779595b8c6
suysh-msra <84946358+suysh-msra@users.noreply.github.com>
2026-08-28 00:31:42 +0530

    Create control_unit.sv

---
+++ pipelined_cpu/control_unit.sv
@@ -0,0 +1,7 @@
+//main +ALU decode for RV32I subset
+//R-type add/sub/and/or/slt, 
+//addi , lw, sw, beq and jal
+
+//rd field sits at the same bit pos [instr[11:7]], so reg_dst mux not needed,
+//as opposed to MIPS archi
+
