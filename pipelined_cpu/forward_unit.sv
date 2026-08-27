c916313c6605d0691b3750116776dd7d94bf55ae
suysh-msra <84946358+suysh-msra@users.noreply.github.com>
2026-08-28 01:50:29 +0530

    Create forward_unit.sv skeleton, needs meat

---
+++ pipelined_cpu/forward_unit.sv
@@ -0,0 +1,10 @@
+//EX stage operand forwarding.
+//selects, for rs1 and rs2,
+//among:
+// regfile value read back in ID
+// mem/wb stage's Write-back value, and
+//EX/MEM stage's ALU rsult (more recent, so takes priority over MEM/Wb stage)
+
+//
+module forward_unit ();
+endmodule
